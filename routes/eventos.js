90ead892e270a4761a8b9b9521adb42ad596efb4
Jonatan Alexis Rodriguez <jonatanr72@gmail.com>
2025-09-18 22:29:47 -0300

    Agregando route faltante

--- routes/eventos.js
+++ routes/eventos.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const eventoController = require("../controllers/eventoController");
 
 router.get("/", eventoController.traer);
+router.get("/completos", eventoController.traerConProveedores);
+router.get("/:id/completo", eventoController.obtenerEventoCompleto);
 router.get("/:id", eventoController.obtenerPorId);
 router.post("/", eventoController.crear);
 router.put("/:id", eventoController.actualizar);
 router.delete("/:id", eventoController.eliminar);
-router.get("/:id/completo", eventoController.obtenerEventoCompleto);
 
 module.exports = router;
